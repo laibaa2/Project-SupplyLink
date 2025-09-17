a03b02074033be017046efa9f5418ae861deca65
Laiba Qureshi <136991854+laibaa2@users.noreply.github.com>
2025-09-17 17:27:00 +0530

    19

--- frontend/src/app/supplylink/components/warehouse/warehouse.component.ts
+++ frontend/src/app/supplylink/components/warehouse/warehouse.component.ts
@@ -1,5 +1,16 @@
 
+import { Component } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+
+@Component ({
+    selector : 'app-warehouse',
+    template : './warehouse.component.html',
+    styleUrls : ['./warehouse.component.scss']
+})
+
 export class WarehouseComponent {
- 
+
+    
+    constructor (private fb : FormBuilder) {}
 
 }
