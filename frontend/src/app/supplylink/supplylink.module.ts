b652fc26abf63a5df17a9c334b4121fa78435ba9
Laiba Qureshi <136991854+laibaa2@users.noreply.github.com>
2025-09-20 12:45:07 +0530

    22

--- frontend/src/app/supplylink/supplylink.module.ts
+++ frontend/src/app/supplylink/supplylink.module.ts
@@ -1,9 +1,13 @@
+
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { SupplyLinkRoutingModule } from "./supplylink-routing.module";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { ProductComponent } from "./components/product/product.component";
+import { SupplierComponent } from "./components/supplier/supplier.component";
+import { WarehouseComponent } from "./components/warehouse/warehouse.component";
 
 @NgModule({
   declarations: [],
@@ -12,6 +16,9 @@ import { HttpClientModule } from "@angular/common/http";
     SupplyLinkRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
+    ProductComponent,
+    SupplierComponent,
+    WarehouseComponent
   ],
   exports: [
     
