89ccd3b6c4b2b469e0930377c2e9f14dd6d8a6ef
Laiba Qureshi <136991854+laibaa2@users.noreply.github.com>
2025-09-22 17:10:15 +0530

    day 25

--- frontend/src/app/auth/components/user/user.component.ts
+++ frontend/src/app/auth/components/user/user.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { AuthService } from "../../services/auth.service";
